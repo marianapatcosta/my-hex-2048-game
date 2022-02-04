b87f99151c08573b5395fde2a4317be8a3b7d772
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2022-02-04 23:32:55 +0000

    add minor fixes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GameBoard } from '..'
+import Logo from '../../assets/hex-2048-logo.svg'
 import './index.module.scss'
 
 export const App: React.FC = () => {
@@ -7,7 +8,7 @@ export const App: React.FC = () => {
     <>
       <header>
         <h2>
-          Hex <img src='/hex-2048-logo.svg' alt='hex 2048 logo' />
+          Hex <img src={Logo} alt='hex 2048 logo' />
         </h2>
       </header>
       <main>
